811d761f62493af21138463521463964f9d6a81a
yeyu-gon <2804476900@qq.com>
2023-06-12 22:30:57 +0800

    feat: 菜单

--- src/layout/Menu/index.vue
+++ src/layout/Menu/index.vue
@@ -10,19 +10,31 @@
     router
     unique-opened
   >
-    <el-sub-menu :index="item.id" v-for="item in menusList" :key="item.id">
+    <el-sub-menu
+      :index="item.id"
+      v-for="(item, index) in menusList"
+      :key="item.id"
+    >
       <template #title>
-        <el-icon><location /></el-icon>
+        <el-icon>
+          <component :is="iconList[index]"></component>
+        </el-icon>
         <span>{{ item.authName }}</span>
       </template>
       <el-menu-item-group>
         <el-menu-item
-          :route="item1.path"
+          :index="item1.path"
           v-for="item1 in item.children"
           :key="item1.id"
           @click="savePath(item1.path)"
-          >{{ item1.authName }}</el-menu-item
         >
+          <template #title>
+            <el-icon>
+              <component :is="icon"></component>
+            </el-icon>
+            <span>{{ item1.authName }}</span>
+          </template>
+        </el-menu-item>
       </el-menu-item-group>
     </el-sub-menu>
   </el-menu>
@@ -35,6 +47,9 @@ import { menuList } from '@/api/menu'
 const defaultActive = ref(sessionStorage.getItem('path') || '/user')
 
 const menusList = ref([])
+
+const iconList = ref(['user', 'setting', 'shop', 'tickets', 'pie-chart'])
+const icon = ref('menu')
 
 const initMenuList = async () => {
   const res = await menuList()
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ import store from './store'
 import '@/styles/index.scss'
 import SvgIcon from '@/icons'
 import '@/router/permission'
+import * as EleIcon from '@element-plus/icons-vue'
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(EleIcon)) {
+  app.component(key, component)
+}
 SvgIcon(app)
 
 app.use(store).use(router).mount('#app')
